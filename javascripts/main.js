cb08589f9f1a6a3f1c42b6b5239d235b47ea04de
Jesse Jurman <j.r.jurman@gmail.com>
2014-12-30 17:39:47 -0500

    commit early; commit often\!

---
+++ javascripts/main.js
@@ -0,0 +1,69 @@
+function generateJSON() {
+  var gameJSON = {};
+  gameJSON["move_types"] = {};
+  gameJSON["characters"] = [];
+
+  var moveTypes = document.querySelectorAll(".moveTypeDiv")
+  Array.prototype.forEach.call(moveTypes, function(e) {
+    gameJSON["move_types"][e.children[0].value] = e.children[1].value;
+  });
+
+  return gameJSON;
+}
+
+function addCharacter() {
+  var character = document.createElement("div");
+  character.className = "character";
+  var characterName = document.createElement("input");
+  characterName.className = "nameInput"
+  characterName.placeholder = "Name";
+  character.appendChild(characterName);
+  addMove(character);
+  var parent = document.getElementById("characters");
+  parent.appendChild(character);
+}
+
+function addMoveType() {
+  var mType = document.createElement("div");
+  mType.className = "moveTypeDiv"
+
+  var mTypeInput = document.createElement("input");
+  mTypeInput.className = "moveType";
+  mTypeInput.placeholder = "Move Type";
+  mType.appendChild(mTypeInput);
+
+  var mTypeColor = document.createElement("select");
+  mTypeColor.className = "moveColor";
+  var colorOptions = mTypeColor.options;
+  colorOptions[0] = new Option("Placeholder 1", "Placeholder 1");
+  colorOptions[1] = new Option("Placeholder 2", "Placeholder 2");
+  colorOptions[2] = new Option("Placeholder 3", "Placeholder 3");
+  mType.appendChild(mTypeColor)
+
+  var parent = document.getElementById("moveTypes");
+  parent.appendChild(mType);
+}
+
+function addMove(parent) {
+  var move = document.createElement("div");
+  move.className = "move";
+  var moveName = document.createElement("input");
+  moveName.placeholder = "Move Name"
+  var moveTypeSelect = document.createElement("select");
+  moveTypeSelect.onfocus = function(){
+    var op = moveTypeSelect.options;
+    var moveTypes = document.getElementsByClassName("moveType");
+    for(moveType in moveTypes){
+      op[parseInt(moveType)] = new Option(moveTypes[moveType].value, moveTypes[moveType].value)
+    }
+  }
+  var moveExec = document.createElement("div");
+  moveExec.className = "moveExec";
+  var mExecInput = document.createElement("input");
+  mExecInput.className = "exec";
+  mExecInput.placeholder = "Move Execution";
+  move.appendChild(moveName);
+  move.appendChild(moveTypeSelect);
+  move.appendChild(mExecInput);
+  parent.appendChild(move);
+}
