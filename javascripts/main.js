083770f276d100e9520c8599f12b329bc279a6ee
Ethan H. Jurman <ehj2229@rit.edu>
2014-12-31 18:24:24 -0500

    styling delete and add move buttons

--- javascripts/main.js
+++ javascripts/main.js
@@ -8,6 +8,22 @@ function generateJSON() {
     gameJSON["move_types"][e.children[0].value] = e.children[1].value;
   });
 
+  var characters = document.getElementsByClassName("character");
+  Array.prototype.forEach.call(characters, function(htmlCharacter){
+    var c = {};
+    c.name = htmlCharacter.getElementsByClassName("nameInput")[0].value;
+    c.moves = [];
+    moves = htmlCharacter.getElementsByClassName("moveWrapping");
+    Array.prototype.forEach.call(moves, function(htmlMove){
+      var moveObject = {};
+      moveObject.name = htmlMove.getElementsByClassName("moveName")[0].value;
+      moveObject.exec = htmlMove.getElementsByClassName("exec")[0].value;
+      moveObject.note = htmlMove.getElementsByClassName("note")[0].value;
+      moveObject.prereq = htmlMove.getElementsByClassName("prereq")[0].value;
+      c.moves.push(moveObject);
+    });
+    gameJSON["characters"].push(c);
+  });
   return gameJSON;
 }
 
@@ -25,8 +41,8 @@ function addCharacter() {
   }
   moveAdder.appendChild(document.createTextNode("add move"));
   var parent = document.getElementById("characters");
-  parent.appendChild(character);
   parent.appendChild(moveAdder);
+  parent.appendChild(character);
   parent.appendChild(document.createElement("hr"));
 }
 
@@ -35,16 +51,16 @@ function addMoveType() {
   mType.className = "moveTypeDiv"
 
   var mTypeInput = document.createElement("input");
-  mTypeInput.className = "moveType";
+  mTypeInput.className = "moveTypeOption";
   mTypeInput.placeholder = "Move Type";
   mType.appendChild(mTypeInput);
 
   var mTypeColor = document.createElement("select");
   mTypeColor.className = "moveColor";
   var colorOptions = mTypeColor.options;
-  colorOptions[0] = new Option("Placeholder 1", "Placeholder 1");
-  colorOptions[1] = new Option("Placeholder 2", "Placeholder 2");
-  colorOptions[2] = new Option("Placeholder 3", "Placeholder 3");
+  colorOptions[0] = new Option("green", "green");
+  colorOptions[1] = new Option("blue", "blue");
+  colorOptions[2] = new Option("red", "red");
   mType.appendChild(mTypeColor)
 
   var parent = document.getElementById("moveTypes");
@@ -55,32 +71,47 @@ function addMove(parent) {
   var move = document.createElement("div");
   move.className = "move";
   var moveName = document.createElement("input");
-  moveName.placeholder = "Move Name"
+  moveName.className = "moveName";
+  moveName.placeholder = "Move Name";
   var moveTypeSelect = document.createElement("select");
+  moveTypeSelect.className = "moveType";
   moveTypeSelect.onfocus = function(){
     var op = moveTypeSelect.options;
-    var moveTypes = document.getElementsByClassName("moveType");
+    var moveTypes = document.getElementsByClassName("moveTypeOption");
     for(moveType in moveTypes){
       op[parseInt(moveType)] = new Option(moveTypes[moveType].value, moveTypes[moveType].value)
     }
   }
-  var moveExec = document.createElement("div");
-  moveExec.className = "moveExec";
   var mExecInput = document.createElement("input");
   mExecInput.className = "exec";
   mExecInput.placeholder = "Move Execution";
   var moveDescription = document.createElement("div");
   moveDescription.className = "moveDescription";
   var moveNote = document.createElement("input");
+  moveNote.className = "note";
   moveNote.placeholder = "move notes (e.g. \"AIR OKAY\")";
   var movePrereq = document.createElement("input");
+  movePrereq.className = "prereq";
   movePrereq.placeholder = "prerequisites (seperated by \"&\")";
   moveDescription.appendChild(moveNote);
   moveDescription.appendChild(movePrereq);
 
+  var removeMove = document.createElement("button");
+  removeMove.appendChild(document.createTextNode("Delete Move"));
+  removeMove.className = "deleteMove";
+  removeMove.onclick = function(){
+    moveWrapping.remove();
+  }
+
+  // wrap all the move elements in one div
+  var moveWrapping = document.createElement("div");
+  moveWrapping.className = "moveWrapping";
+
   move.appendChild(moveName);
   move.appendChild(moveTypeSelect);
   move.appendChild(mExecInput);
-  parent.appendChild(move);
-  parent.appendChild(moveDescription);
+  moveWrapping.appendChild(removeMove);
+  moveWrapping.appendChild(move);
+  moveWrapping.appendChild(moveDescription);
+  parent.appendChild(moveWrapping);
 }
