b17a43a36be10afb15d030ff31018369098206c5
Ethan H. Jurman <ehj2229@rit.edu>
2015-01-05 13:37:40 -0500

    can load json

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,6 +18,7 @@ function generateJSON() {
     Array.prototype.forEach.call(moves, function(htmlMove){
       var moveObject = {};
       moveObject.name = htmlMove.getElementsByClassName("moveName")[0].value;
+      moveObject.type = htmlMove.getElementsByClassName("moveType")[0].value
       moveObject.exec = htmlMove.getElementsByClassName("exec")[0].value;
       moveObject.note = htmlMove.getElementsByClassName("note")[0].value;
       moveObject.prereq = htmlMove.getElementsByClassName("prereq")[0].value;
@@ -29,27 +30,63 @@ function generateJSON() {
   return gameJSON;
 }
 
-function addMoveType() {
+function loadJSON() {
+  var gameJSON = JSON.parse(document.getElementById("json-textarea").value);
+  document.getElementById("game").value = gameJSON["game_name"];
+  for (i in gameJSON["move_types"]){
+    addMoveType({"moveTypeOption":i, "moveColor":gameJSON["move_types"][i]});
+  }
+  for (i in gameJSON["characters"]){
+    var cJSON = gameJSON["characters"][i];
+    addCharacter(
+      {"nameInput":cJSON.name},
+      function(cJSON){
+        console.log(cJSON);
+        for (m in cJSON.moves){
+          var move = cJSON.moves[m];
+          // parentHTML provided by method that calls this function
+          addMove(cJSON.parentHTML, {
+            "moveName":move.name,
+            "moveType":move.type,
+            "exec":move.exec,
+            "note":move.note,
+            "prereq":move.prereq
+          });
+        }
+    }, cJSON);
+  }
+}
+
+function addMoveType(loadValues, loadFunc, loadParams) {
   loadExternalHtml(
     "htmlTemplates/moveType.html",
     "moveTypeDiv",
-    document.getElementById("moveTypes")
+    document.getElementById("moveTypes"),
+    loadValues,
+    loadFunc,
+    loadParams
   );
 }
 
-function addCharacter() {
+function addCharacter(loadValues, loadFunc, loadParams) {
   loadExternalHtml(
     "htmlTemplates/character.html",
     "character",
-    document.getElementById("characters")
+    document.getElementById("characters"),
+    loadValues,
+    loadFunc,
+    loadParams
   );
 }
 
-function addMove(parent) {
+function addMove(parent, loadValues, loadFunc, loadParams) {
   loadExternalHtml(
     "htmlTemplates/move.html",
     "move",
-    parent
+    parent,
+    loadValues,
+    loadFunc,
+    loadParams
   );
 }
 
@@ -83,15 +120,28 @@ function updateMoveSelection(e){
   }
 }
 
-function loadExternalHtml(page, divClass, location){
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.open("GET",page,true);
-  xmlhttp.send();
-  xmlhttp.onreadystatechange = function(){
-    if (xmlhttp.readyState==4){
+function loadExternalHtml(page, divClass, location, loadValues, loadFunc, loadParams){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET",page,true);
+  xhr.send();
+  xhr.onreadystatechange = function(){
+    if (xhr.readyState==4){
       loaded = document.createElement("div");
       loaded.className = divClass;
-      loaded.innerHTML = xmlhttp.responseText;
+      loaded.innerHTML = xhr.responseText;
+      for (v in loadValues){
+        loaded.getElementsByClassName(v)[0].setAttribute("value",loadValues[v]);
+        Array.prototype.forEach.call(loaded.getElementsByTagName("option"),
+          function(option){
+            if (option.value == loadValues[v]){
+              option.setAttribute("selected","selected");
+            }
+          });
+        if (loadFunc !== undefined){
+          loadParams.parentHTML = loaded; // +parent information to parameters
+          loadFunc(loadParams)
+        }
+      }
       location.appendChild(loaded);
     }
   }
