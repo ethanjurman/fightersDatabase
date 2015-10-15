d724887f1cb235c0ec2fda14a586bf72fce2d11e
Ethan H. Jurman <ehj2229@rit.edu>
2015-10-15 14:28:30 -0400

    updated to work with external application as module

--- app.js
+++ app.js
@@ -3,26 +3,17 @@
 	Created by: Ethan Jurman
 */
 
-var express = require('express')
-var app = require('express')();
-var http = require('http').Server(app);
-var cors = require('cors');
-
-app.use(cors()); // I hate cors
+var express = require('express');
+var router = express.Router();
 
 // Load the games json (has all the game names and groups)
-app.get("/games", function(req, res){
+router.get("/games", function(req, res){
   res.sendFile(__dirname + '/games.json');
 });
 
 // Load specific game from games folder
-app.get("/games/:game", function(req, res) {
+router.get("/games/:game", function(req, res) {
   res.sendFile(__dirname + '/games/' + req.params.game + '.json');
 });
 
-// Serve up them files!
-http.listen(9000, function () {
-  console.log('Up and running');
-});
-
-module.export = app;
+module.exports = router;
